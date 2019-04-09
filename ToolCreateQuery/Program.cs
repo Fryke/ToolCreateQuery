d1e173e771ece91f3f04682be254db0dd69d04d2
Fryke <26737110+Fryke@users.noreply.github.com>
2019-04-09 11:44:19 +0200

    Function to get all the fil names in the destionation folder

--- ToolCreateQuery/Program.cs
+++ ToolCreateQuery/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,20 @@ namespace ToolCreateQuery
 {
     class Program
     {
+        //Function to get all file names in folder
+        private static List<string> GetListCountry()
+        {
+            DirectoryInfo d = new DirectoryInfo(@"C:\Users\%USERNAME%\Pictures\Flags");
+            FileInfo[] files = d.GetFiles("*.png");
+            List<string> lstNames = new List<string>();
+            foreach (var file in files)
+            {
+                lstNames.Add(file.Name.Replace(".png", String.Empty)); //NL.png->NL
+            }
+
+            return lstNames;
+        }
+
         static void Main(string[] args)
         {
         }
