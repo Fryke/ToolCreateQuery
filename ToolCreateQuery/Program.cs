3264190ce6913acc2389b5df6a8fe97acf14f649
Fryke <26737110+Fryke@users.noreply.github.com>
2019-04-09 11:46:07 +0200

    Generate the INSERT query for the Question database

--- ToolCreateQuery/Program.cs
+++ ToolCreateQuery/Program.cs
@@ -37,6 +37,29 @@ namespace ToolCreateQuery
             return myHashSet.OrderBy(s => Guid.NewGuid()).ToList();
         }
 
+        //Function to generate query insert into for create Database Question
+        private static async Task GenerateQuery()
+        {
+            List<string> lstQuery = new List<string>();
+            List<string> lstCountryName = GetListCountry();
+            string query = string.Empty;
+            foreach (var name in lstCountryName)
+            {
+                //With one value in lstCountryname, we create one question with 4 aswers and 1 correct answer
+                List<string> answerList = GetNameRandom(name, lstCountryName);
+
+                query = "INSERT INTO Question(Image,AnswerA,AnswerB,AnswerC,AnswerD,CorrectAnswer)"
+                        + $"VALUES(\"{name}\",\"{answerList[0]}\",\"{answerList[1]}\",\"{answerList[2]}\",\"{answerList[3]}\",\"{name}\");";
+                //Ex
+                //INSERT INTO Question(Image,AnswerA,AnswerB,AnswerC,AnswerD,CorrectAnswer)
+                //VALUES('NL','DE','BE','UK,'FR','NL');
+
+                lstQuery.Add(query); //add query we just create to list
+            }
+            //Write all to file
+            System.IO.File.WriteAllLines(@".//QueryGenerate.txt", lstQuery);
+        }
+
         static void Main(string[] args)
         {
         }
