c7ccb3a96cbc795f44b2df97054df9a8ea5568c0
Fryke <26737110+Fryke@users.noreply.github.com>
2019-04-09 11:45:07 +0200

    Get a random name of the items in the folder, without duplicates

--- ToolCreateQuery/Program.cs
+++ ToolCreateQuery/Program.cs
@@ -23,6 +23,20 @@ namespace ToolCreateQuery
             return lstNames;
         }
 
+        //Function get random name of country from list without duplicate
+        private static List<string> GetNameRandom(string name, List<string> lstNames)
+        {
+            HashSet<string> myHashSet = new HashSet<string>();
+            myHashSet.Add(name); //first add name
+            while (myHashSet.Count < 4) //4 names for 4 answers
+            {
+                //Add random name to HashSet
+                myHashSet.Add(lstNames.OrderBy(s => Guid.NewGuid()).First());
+            }
+
+            return myHashSet.OrderBy(s => Guid.NewGuid()).ToList();
+        }
+
         static void Main(string[] args)
         {
         }
